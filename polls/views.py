4cd498dca9e0642ae9848cda9686219659c9e138
Nuafah <yasatsawin.k@ku.th>
2023-09-17 19:37:50 +0700

    Implement Sign up

--- polls/views.py
+++ polls/views.py
@@ -5,6 +5,8 @@ from django.views import generic
 from django.utils import timezone
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 
 from .models import Choice, Question, Vote
 
@@ -83,7 +85,26 @@ def vote(request, question_id):
         # create a new vote and save
         vote = Vote(user=user, choice=selected_choice)
     vote.save()
-    # TODO: add confirmation message
     messages.success(request, message=f"voted for {selected_choice}")
     return HttpResponseRedirect(reverse('polls:results',
                                         args=(question.id,)))
+
+def signup(request):
+    """Register a new user."""
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # get named fields from the form data
+            username = form.cleaned_data.get('username')
+            # password input field is named 'password1'
+            raw_passwd = form.cleaned_data.get('password1')
+            user = authenticate(username=username,password=raw_passwd)
+            login(request, user)
+            return redirect('polls:index')
+        # what if form is not valid?
+        # we should display a message in signup.html
+    else:
+        # create a user form and display it the signup page
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
